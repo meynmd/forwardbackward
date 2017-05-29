69bcb2b3962b901b750a5a119164d63392ba5ee8
dimbul <durgaharish1993@gmail.com>
2017-05-29 15:50:19 -0700

    Final FB

--- fb.py
+++ fb.py
@@ -1,5 +1,6 @@
 from __future__ import print_function
 from collections import defaultdict
+import numpy as np
 
 '''
 Expectation
@@ -11,19 +12,111 @@ prior:      dict mapping e->j->p where p is current prob. est.
 maxE2J:     maximum k in k-to-1 mapping
 '''
 def Expectation(wordPairs, prior, maxE2J):
-    fracCount = defaultdict(lambda : defaultdict(float))
-    for eword, jword in wordPairs:
+    fracCount = {}
+    for i in range(len(wordPairs)):
+        eword = wordPairs[i][0]
+        jword = wordPairs[i][1]
         e, j = eword.split(), jword.split()
         # compute forward and backward prob for each word
         alpha = Forward(e, j, prior, maxE2J)
         beta = Backward(e, j, prior, maxE2J)
+
         # sum the fractional counts
-        c = FindFracCounts(e, j, alpha, beta, prior, maxE2J)
-        for ep, js_co in c.items():
-            for js, co in js_co.items():
-                fracCount[ep][js] += co
+        fracCount[i] = FindFracCounts(e, j, alpha, beta, prior, maxE2J)
 
     return fracCount
+
+
+'''
+FindFracCounts
+eprons:     list of English sounds
+jprons:     list of Japanese sounds
+alpha:      array of forward probabilities by [epron][jpron]
+beta:       array of backward probabilities
+prior:      dict mapping e->j->p where p is current prob. est.
+maxE2J:     maximum k in k-to-1 mapping
+'''
+
+
+def FindFracCounts(eprons, jprons, alpha, beta, prior, maxE2J):
+    numJ, numE = len(jprons), len(eprons)
+    # counts = defaultdict(lambda : defaultdict(float))
+
+
+
+    eplison = defaultdict(lambda: defaultdict(lambda: defaultdict(float)))
+
+    for i in range(1,numE+1):
+        prev_alpha = np.array(alpha[i])
+        cur_alpha = np.array(alpha[i - 1])
+
+        cur_indexes = list(np.where(prev_alpha > 0)[0])
+        pre_indexes = list(np.where(cur_alpha > 0)[0])
+        ### I will be doing more work but its fine.  I will be visiting more edges which zero probability.
+        for i_1 in pre_indexes:
+            for i_2 in cur_indexes:
+                ep = eprons[i-1]
+                jp = tuple(jprons[i_1:i_2])
+                prob = (alpha[i-1][i_1] * prior[ep][jp] * beta[i][i_2]) /alpha[numE][numJ]
+                if prob > 0:
+                    eplison[i][ep][jp] = prob
+
+
+    return eplison
+
+
+
+
+
+
+    #
+    #
+    # for a in EnumAligns(eprons, jprons, []):
+    #     e, j = 0, 0
+    #     p_x_z = 1.
+    #     for (ep, js) in a:
+    #         e += 1
+    #         j += len(js)
+    #         #p(x | z) : whole word alignment prob
+    #         p_x_z *= alpha[e][j] * beta[e][j] * prior[ep][js]
+    #
+    #         counts[ep][js] += 1
+    #
+    # # now we need to do something with p_x_z
+    #
+    #
+    # # normalize...?
+    # for e, d in counts.items():
+    #     for j in d.keys():
+    #         counts[e][j] /= beta[1][1]
+
+
+
+
+
+def Maximization(wordPairs, fracCount, prior):
+    updated_prior = defaultdict(lambda :defaultdict(float))
+    sum_prob = defaultdict(float)
+
+    for i in range(len(wordPairs)):
+        for t in fracCount[i]:
+            for key in fracCount[i][t]:
+                for key1 in fracCount[i][t][key]:
+                    updated_prior[key][key1] += fracCount[i][t][key][key1]
+                    sum_prob[key]+=fracCount[i][t][key][key1]
+
+    for key in updated_prior:
+        for key1 in updated_prior[key]:
+            updated_prior[key][key1] =  updated_prior[key][key1]/sum_prob[key]
+
+
+    return updated_prior
+
+
+
+
+
+
 
 
 
@@ -49,7 +142,7 @@ maxE2J:     maximum k in k-to-1 mapping
 def Forward(eprons, jprons, prior, maxE2J):
     # alpha: prob. of getting to this state by some path from start
     numJ, numE = len(jprons), len(eprons)
-    alpha = [[0. for i in range(numJ + 2)] for j in range(numE + 2)]
+    alpha = [[0. for i in range(numJ + 1)] for j in range(numE + 1)]
     alpha[0][0] = 1.
     # find alpha for each alignment
     for i in range(numE):
@@ -68,7 +161,7 @@ backward
 eprons:     list of English sounds
 jprons:     list of Japanese sounds
 prior:      dict mapping e->j->p where p is current prob. est.
-maxE2J:     maximum k in k-to-1 mapping
+maxE2J:     maximum k in k-to-1 mappings
 '''
 def Backward(eprons, jprons, prior, maxE2J):
     # beta: prob. of getting to final state from this state
@@ -81,48 +174,14 @@ def Backward(eprons, jprons, prior, maxE2J):
                 ep, js = eprons[i], tuple(jprons[j - k : j + 1])
                 beta[i][j - k] += beta[i + 1][j + 1] * prior[ep][js]
 
-    for row in beta:
-        row.insert(0, 0)
-    beta.insert(0, [0 for x in beta[-1]])
+    # for row in beta:
+    #     row.insert(0, 0)
+    # beta.insert(0, [0 for x in beta[-1]])
     return beta
     #return [row[: numJ] for row in beta[: numE]]
 
 
 
-'''
-FindFracCounts
-eprons:     list of English sounds
-jprons:     list of Japanese sounds
-alpha:      array of forward probabilities by [epron][jpron]
-beta:       array of backward probabilities
-prior:      dict mapping e->j->p where p is current prob. est.
-maxE2J:     maximum k in k-to-1 mapping
-'''
-def FindFracCounts(eprons, jprons, alpha, beta, prior, maxE2J):
-    numJ, numE = len(jprons), len(eprons)
-    counts = defaultdict(lambda : defaultdict(float))
-
-    alignProbs = {}
-    for a in EnumAligns(eprons, jprons, []):
-        e, j = 0, 0
-        p_x_z = 1.
-        for (ep, js) in a:
-            e += 1
-            j += len(js)
-            #p(x | z) : whole word alignment prob
-            p_x_z *= alpha[e][j] * beta[e][j] * prior[ep][js]
-
-            counts[ep][js] += 1
-
-    # now we need to do something with p_x_z
-
-
-    # normalize...?
-    for e, d in counts.items():
-        for j in d.keys():
-            counts[e][j] /= beta[1][1]
-
-    return counts
 
 
 
@@ -197,17 +256,28 @@ def InitProb(pairs):
 if __name__ == '__main__':
     fname = 'data/epron-jpron.data'
     pairs = ReadEpronJpron(fname)
-    probs = InitProb(pairs)
-    counts = Expectation(pairs, probs, 3)
+    #pairs = [('W AY N','W A I N')]
+    prior = InitProb(pairs)
 
-    # (e, j) = pairs[0]
-    # e, j = e.split(), j.split()
-    # aligns = EnumAligns(e, j, [])
-    # for a in aligns:
-    #     print('{}'.format(a))
 
-    #we need to do the maximization step, but for now I'll print the fractional counts
-    for e, jp in counts.items():
-        print('\n*** {} ***'.format(e))
-        for j, p in jp.items():
-            print('\t\t{} : {}'.format(j, p))
+    iters = 2
+
+    Uprior = prior
+    for i in range(iters):
+
+        prior = Uprior
+        print('iteration ', i, ' ----- ')
+        #########
+        count = 0
+        for key in prior:
+            print(key, '|->', end=' ')
+            for key1 in prior[key]:
+                if prior[key][key1] > 0.001:
+                    count += 1
+                    print(' '.join(key1), ':', round(prior[key][key1], 3), end=' ')
+            print('\n')
+        print('nonzeros =', count)
+        ##########
+
+        counts = Expectation(pairs, prior, 3)
+        Uprior = Maximization(pairs, counts, prior)
